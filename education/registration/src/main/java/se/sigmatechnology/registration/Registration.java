45d1c39bd89afe19cd250d1e8c6117ac98a32667
Pezhman Kasraee <pezhman.kasraee@sigmatechnology.se>
2017-10-09 10:10:21 +0200

    ADD: project converted to a multi module project

---
+++ education/registration/src/main/java/se/sigmatechnology/registration/Registration.java
@@ -0,0 +1,17 @@
+package se.sigmatechnology.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Registration
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Registration.class);
+    }
+}
--- education/registration/src/test/java/se/sigmatechnology/registration/RegistrationTest.java
+++ education/registration/src/test/java/se/sigmatechnology/registration/RegistrationTest.java
@@ -1,4 +1,4 @@
-package se.sigmatechnology;
+package se.sigmatechnology.registration;
 
 import junit.framework.Test;
 import junit.framework.TestCase;

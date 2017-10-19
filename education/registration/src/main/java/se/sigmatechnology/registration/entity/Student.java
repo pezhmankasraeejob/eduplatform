2d1f4a375c5cf9ae23f9c2d45c7bfba6060e2af1
Pezhman Kasraee <pezhman.kasraee@sigmatechnology.se>
2017-10-19 11:13:59 +0200

    IMP: Student code improved
         - Unwanted parts of code removed

--- education/registration/src/main/java/se/sigmatechnology/registration/entity/Student.java
+++ education/registration/src/main/java/se/sigmatechnology/registration/entity/Student.java
@@ -2,6 +2,7 @@ package se.sigmatechnology.registration.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
@@ -9,9 +10,10 @@ import java.util.Date;
  * Created by pke on 2017-10-10.
  */
 @Entity
+@Table(name = "tblStudent")
 public class Student {
-    @NotNull
     @Id
+    @NotNull
     private String id;
     @NotNull
     private String fullName;
